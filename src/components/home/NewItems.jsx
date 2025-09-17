ead6d49f22916ef5dacc211b45b6011ad42de472
mojie00 <116626655+mojie00@users.noreply.github.com>
2025-09-17 11:50:17 -0400

    added topsellers data

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Countdown from "./Countdown"; // Adjust the import based on your file structure
+import Skeleton from "../UI/Skeleton";
 
 
 
@@ -29,64 +30,85 @@ useEffect(() => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {data.map((elem, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title={`Creator: ${elem.Creator}`}
-                  >
-                    <img className="lazy" src={elem.authorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">
-                  <Countdown expiryDate={elem.expiryDate} />
-                </div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
+          {data.length === 0 ? (
+            // Skeleton loading state
+            Array.from({ length: 4 }).map((_, idx) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={idx}>
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Skeleton width={50} height={50} borderRadius={"50%"} />
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={elem.nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>{elem.title}</h4>
-                  </Link>
-                  <div className="nft__item_price">{elem.price} ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>{elem.likes}</span>
+                  <div className="de_countdown">
+                    <Skeleton width={100} height={20} borderRadius={8} />
+                  </div>
+                  <div className="nft__item_wrap">
+                    <Skeleton width={250} height={250} borderRadius={16} />
+                  </div>
+                  <div className="nft__item_info">
+                    <Skeleton width={120} height={20} borderRadius={8} />
+                    <Skeleton width={60} height={20} borderRadius={8} />
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            data.map((elem, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Link
+                      to="/author"
+                      data-bs-toggle="tooltip"
+                      data-bs-placement="top"
+                      title={`Creator: ${elem.Creator}`}
+                    >
+                      <img className="lazy" src={elem.authorImage} alt="" />
+                      <i className="fa fa-check"></i>
+                    </Link>
+                  </div>
+                  <div className="de_countdown">
+                    <Countdown expiryDate={elem.expiryDate} />
+                  </div>
+                  <div className="nft__item_wrap">
+                    <div className="nft__item_extra">
+                      <div className="nft__item_buttons">
+                        <button>Buy Now</button>
+                        <div className="nft__item_share">
+                          <h4>Share</h4>
+                          <a href="" target="_blank" rel="noreferrer">
+                            <i className="fa fa-facebook fa-lg"></i>
+                          </a>
+                          <a href="" target="_blank" rel="noreferrer">
+                            <i className="fa fa-twitter fa-lg"></i>
+                          </a>
+                          <a href="">
+                            <i className="fa fa-envelope fa-lg"></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                    <Link to="/item-details">
+                      <img
+                        src={elem.nftImage}
+                        className="lazy nft__item_preview"
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft__item_info">
+                    <Link to="/item-details">
+                      <h4>{elem.title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{elem.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{elem.likes}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </section>
--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+  const [data, setData] = React.useState([]);
+
+  async function getData() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+    );
+    setData(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +29,21 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {data.map((elem, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
                     <Link to="/author">
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={elem.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">{elem.authorName}</Link>
+                    <span>{elem.price} ETH</span>
                   </div>
                 </li>
               ))}
